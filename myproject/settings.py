6d060f41576ad560e70f5636b29dcc1fd543e9be
MADESHKANNA <madeshkanna2005@gmail.com>
2025-12-11 08:59:41 +0530

    Refactor static file settings in settings.py
    Updated STATIC_URL and removed static file settings.

--- myproject/settings.py
+++ myproject/settings.py
@@ -116,10 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'recipes' / 'static'
-]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
